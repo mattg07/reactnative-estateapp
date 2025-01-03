92d6b13be748eb8a759ad13213ef5f1e5af36686
mattg07 <matiasmgodoy00@gmail.com>
2025-01-03 22:49:54 +0100

    routes and first signin view

--- app/signin.tsx
+++ app/signin.tsx
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To EState
+          </Text>
+          <Text className="text-3xl font-rubik-bold">
+            Lets Get You Closer to{" "}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <TouchableOpacity className=" bg-white  shadow-md mt-10 py-4  shadow-zinc-300 rounded-full w-full">
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+                />
+                <Text>Continue with Google</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default SignIn
+export default SignIn;
