7f66e915a98fb843fbc634913ff40d903a87a1ff
mattg07 <matiasmgodoy00@gmail.com>
2025-01-13 21:05:29 +0100

    single profile screen

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,25 +1,40 @@
 import AddPostForm from "@/components/AddPostForm";
+import ProfileForm from "@/components/ProfileForm";
+import { Profile } from "@/lib/api";
 import { supabase } from "@/lib/supabase";
 import { useUserInfo } from "@/lib/userContext";
-import { useEffect } from "react";
-import { View, Text } from "react-native";
-import { SafeAreaView,  } from "react-native-safe-area-context";
+import { useEffect, useState } from "react";
+import { View, Text, Alert } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Profile(){
-    // useEffect(() => {
-    //     const fetchSinglePost = () => {
-    //             const { data, error } = await supabase.from("Posts").select("*").eq('')            
-    //     }
-    // }, [])
-    const {profile} = useUserInfo()
-    return(
+export default function ProfileScreen() {
+  const [loading, setLoading] = useState(false);
+  const { profile } = useUserInfo();
+
+    const handleSave = async (updatedProfile: Profile) => {
+        setLoading(true)
+        const {data , error} = await supabase.from('profiles').update(updatedProfile).eq('id', profile!.id)
+            if(error){
+                Alert.alert("Server error", error.message)
+            }
+        setLoading(false)
+
+    }
+
+
+  return (
     <SafeAreaView className="bg-background-100 h-full">
-            <View>
-                <Text className="text-white text-xl font-bold">
-                    {profile?.username}
-                </Text>
-
-            </View>
-        </SafeAreaView>
-    )
+        <Text className="text-white text-xl font-bold">
+          {profile?.username}
+        </Text>
+      <ProfileForm
+        profile={profile}
+        loading={false}
+        onSave={handleSave}
+        onLogout={() => supabase.auth.signOut()}
+      />
+      <View>
+      </View>
+    </SafeAreaView>
+  );
 }
