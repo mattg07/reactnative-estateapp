77a23ee7a8f269c2adf5784903d765c7cd56b8e6
mattg07 <matiasmgodoy00@gmail.com>
2025-01-14 22:41:36 +0100

    post images add and RLS

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -5,41 +5,63 @@ import AddPostForm from "@/components/AddPostForm";
 import { fetchPosts, Posts } from "@/lib/api";
 import PostCard from "@/components/PostCard";
 
-
 export default function Explore() {
   const [posts, setPosts] = useState<Posts>([]);
 
   useEffect(() => {
-  
-
-    fetchPosts().then(data => setPosts(data) )
+    fetchPosts().then((data) => setPosts(data));
   }, []);
 
-  const handleSubmit = async (content: string) => {
-    const { data, error } = await supabase
-      .from("Posts")
-      .insert({
-        content,
-      })
-      .select("*, profile: profiles(username, avatar_url)")
-      .order("created_at", { ascending: false });
-    if (error) {
-      console.error(error);
-    } else {
-      setPosts([data[0], ...posts]);
+  const handleSubmit = async (content: string, image: string) => {
+    try {
+      let publicUrl = "";
+      if (image) {
+        const fileExt = image.split(".").pop();
+        const fileName = image.replace(/^.*[\\\/]/, "");
+        const filePath = `${Date.now()}.${fileExt}`;
+
+        const formData = new FormData();
+        const photo = {
+          uri: image,
+          name: fileName,
+          type: `image/${fileExt}`,
+        } as unknown as Blob;
+        formData.append("file", photo);
+
+        const { error } = await supabase.storage
+          .from("posts")
+          .upload(filePath, formData);
+        if (error) throw error;
+
+        const { data } = supabase.storage.from("posts").getPublicUrl(filePath);
+        publicUrl = data.publicUrl;
+      }
+      const { data, error } = await supabase
+        .from("Posts")
+        .insert({
+          content,
+          images: publicUrl
+        })
+        .select("*, profile: profiles(username, avatar_url)")
+        .order("created_at", { ascending: false });
+      if (error) {
+        console.error(error);
+      } else {
+        setPosts([data[0], ...posts]);
+      }
+    } catch (error: any) {
+      Alert.alert("Server error", error.message);
     }
   };
 
-  const handleDeletePost = async (id:string) => {
-const {error, data} = await supabase.from('Posts').delete().eq('id', id)
-    if(error){
-      Alert.alert("Server error", error.message)
+  const handleDeletePost = async (id: string) => {
+    const { error, data } = await supabase.from("Posts").delete().eq("id", id);
+    if (error) {
+      Alert.alert("Server error", error.message);
+    } else {
+      setPosts(posts.filter((post) => post.id !== id));
     }
-    else{
-      setPosts(posts.filter(post => post.id !== id))
-    }
-  
-  }
+  };
 
   console.log(posts);
   return (
@@ -57,8 +79,10 @@ const {error, data} = await supabase.from('Posts').delete().eq('id', id)
         <FlatList
           data={posts}
           keyExtractor={(item) => item.id}
-          contentContainerStyle={{paddingTop: 8}}
-          renderItem={({ item }) => <PostCard post={item} onDelete={()=> handleDeletePost(item.id)} />}
+          contentContainerStyle={{ paddingTop: 8 }}
+          renderItem={({ item }) => (
+            <PostCard post={item} onDelete={() => handleDeletePost(item.id)} />
+          )}
         />
       </View>
     </SafeAreaView>
--- components/AddPostForm.tsx
+++ components/AddPostForm.tsx
@@ -1,50 +1,79 @@
-import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import { useState } from "react";
-import {Feather} from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 interface Props {
-  onSubmit: (content: string) => void;
+  onSubmit: (content: string, image : string) => void;
 }
 
 export default function AddPostForm({ onSubmit }: Props) {
   const [content, setContent] = useState("");
+  const [image, setImage] = useState("");
+
+  const handlePickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    });
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   return (
-    <View style={{
+    <View
+      style={{
         backgroundColor: "",
         borderRadius: 10,
         marginTop: 25,
         marginBottom: 20,
         width: 350,
         padding: 8,
-    
-    }} >
+      }}
+    >
       <Text className="text-white font-semibold p-4">
-      ¿  What are you thinking ?
+        ¿ What are you thinking ?
       </Text>
       <TextInput
         value={content}
         onChangeText={setContent}
         style={styles.input}
       />
-      <View style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-      }} >
-        <TouchableOpacity>
-            <Feather color="white" name="image" size={22}/>
-        </TouchableOpacity>
-      <Button
-      
-        title="Publicar"
-        onPress={() => {
-            onSubmit(content);
-            setContent("");
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
         }}
-        color={"white"}
+      >
+        <TouchableOpacity onPress={handlePickImage}>
+          <Feather color="white" name="image" size={22} />
+        </TouchableOpacity>
+        <Button
+          title="Publicar"
+          onPress={() => {
+            onSubmit(content, image);
+            setContent("");
+            setImage("")
+          }}
+          color={"white"}
         ></Button>
-        </View>
+      </View>
+      {image && (
+        <ImageBackground source={{uri: image}} style={styles.image}>
+          <TouchableOpacity>
+            <Feather name="x"  size={24} color="black"/>
+          </TouchableOpacity>
+        </ImageBackground>
+      )}
     </View>
   );
 }
@@ -57,6 +86,14 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingTop: 5,
     marginTop: 10,
+    color : "white",
     marginBottom: 10,
   },
+  image: {
+    height: 100,
+    width: 100,
+    alignItems:"flex-end",
+    padding: 8,
+
+  }
 });
